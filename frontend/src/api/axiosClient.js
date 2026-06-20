b488fd82a265200aa5c04e82a209377bf9454ddf
sutli19 <jsweety544@gmail.com>
2026-06-20 07:20:25 +0530

    Configure frontend for deployment

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
-  timeout: 10000,
+baseURL:
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api",  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
